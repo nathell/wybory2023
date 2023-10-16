d6ab0c4f6c1175ff5f743f9822f64072526cb31e
Daniel Janus <dj@danieljanus.pl>
2023-10-16 20:21:19 +0200

    Refactor to enable alternative calculations
    For the original and modified Sainte-Laguë. Also, don't tell
    anyone, but I actually never implemented filtering by
    threshold... It ended up not making any difference, anyway.

--- src/wybory2023/dhondt.clj
+++ src/wybory2023/dhondt.clj
@@ -30,15 +30,22 @@
    :td 30244
    :konf 16942})
 
-(defn dhondt [votes seats]
-  (->>
-   (for [[k n] votes]
-     (take 20
-           (map #(vector k (/ n %)) (iterate inc 1))))
-   (merge-sorted second)
-   (take seats)
-   (map first)
-   frequencies))
+(def quotients
+  {:dhondt (iterate inc 1)
+   :sainte-lague (iterate #(+ % 2) 1)
+   :sainte-lague-modified (cons 1.4 (iterate #(+ % 2) 3))})
+
+(defn dhondt
+  ([votes seats] (dhondt :dhondt votes seats))
+  ([method votes seats]
+   (->>
+    (for [[k n] votes]
+      (take 20
+            (map #(vector k (/ n %)) (quotients method))))
+    (merge-sorted second)
+    (take seats)
+    (map first)
+    frequencies)))
 
 (def above-threshold
   #{29624 29617 29625 29615 29621 29643})
@@ -47,14 +54,17 @@
   [12 8 14 12 13 15 12 12 10 9 12 8 14 10 9 10 9 12 20 12
    12 11 15 14 12 14 9 7 9 9 12 9 16 8 10 12 9 9 10 8 12])
 
-(defn calculate [results]
-  (map (fn [{:keys [region results]}]
-         (let [seats (nth seats-per-region (dec region))
-               input (for [{:keys [list votes id]} results]
-                       [(:short-name (parse/committees id)) votes])]
-           {:region region
-            :seats (dhondt input seats)}))
-       results))
+(defn calculate
+  ([results] (calculate :dhondt results))
+  ([method results]
+   (map (fn [{:keys [region results]}]
+          (let [seats (nth seats-per-region (dec region))
+                input (for [{:keys [list votes id]} results
+                            :when (contains? above-threshold id)]
+                        [(:short-name (parse/committees id)) votes])]
+            {:region region
+             :seats (dhondt method input seats)}))
+        results)))
 
 (defn total [results]
   (apply merge-with + (map :seats results)))
